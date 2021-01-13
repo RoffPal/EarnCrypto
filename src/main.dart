c31c956987c2ce4abc186a25964fb13076a7fcbb
RoffPal <45043774+RoffPal@users.noreply.github.com>
2021-01-13 14:49:18 +0100

    Database setUp

--- src/main.dart
+++ src/main.dart
@@ -1,19 +1,77 @@
-import 'dart:io';
-
 import 'package:teledart/teledart.dart';
 import 'package:teledart/telegram.dart';
-import 'package:teledart/model.dart';
+import 'package:firebase/firebase_io.dart';
+import 'util/sensitive_info.dart' as private;
+import 'util/user_model.dart';
+
+
+
+final DB_PATH = private.FIREBASE_PATH;
+final BOT_TOKEN = private.TELEGRAM_BOT_TOKEN;
+final bot = Telegram(BOT_TOKEN);
+FirebaseClient db = FirebaseClient.anonymous();
+String bot_username;
+
 
 void main() {
+  var teledart = TeleDart(bot, Event());
+  teledart.start().then((me){
+    bot_username = me.username;
+    print('${me.username} is initialised');
+  });
 
-  var teledart = TeleDart(Telegram(Platform.environment["BOT_TOKEN"]), Event());
 
-  teledart.start().then((me) => print('${me.username} is initialised'));
-
+// On_Start Command
   teledart
-      .onMessage(keyword: 'Fight for freedom')
-      .listen((message) => message.reply('Ori E ti baje'));
+      .onCommand().listen((event) async {
+        await initializeUser(event.from.id, event.text);
+        event.reply(private.welcomeMsg(event.from.first_name),
+        parse_mode: "markdown",
+        reply_markup: private.showMainMenu());
+      });
 
-  print(Platform.environment["BOT_TOKEN"]);
+
+// Button b clicked
+  teledart
+      .onMessage(keyword: private.b.split(" ")[1].substring(1)).listen((event) async {
+        var user = await getUserDetail(event.chat.id);
+        event.reply("${private.bCheck}*\$${user[private.b.split(" ")[1].toLowerCase()]}*",
+        parse_mode: "markdown",
+        reply_markup: private.showBMenu());
+  });
+
+
+// Button r clicked
+   teledart
+      .onMessage(keyword: private.r.split(" ")[1].substring(1)).listen((event) async {
+        dynamic user = await getUserDetail(event.chat.id);    // looks too complex
+        String reff = private.rCheck(user[private.r.split(" ")[1].toLowerCase()],user[private.rb],user["link"]);
+        event.reply(reff,//private.rCheck(user[private.r.split(" ")[1].toLowerCase()],user[private.rb],user["link"]),
+        parse_mode: "markdown",);
+  });
 
 }
+
+
+
+// Registers new Users into the database
+Future<void> initializeUser(int userID, String msg) async{
+  var ref = msg.split(" ").length > 1 ? msg.split(" ")[1] : null;  // Holds reference to the Id of the upline in the database  
+
+// Here Ensures that a user never gets registered twice by first checking to see if the user already exits
+  if (await db.get("$DB_PATH/Users/$userID.json") == null){      
+      if (ref != null){                                           // Deals with making sure the upline gets its count of referrals updated
+         ref = private.resolveUniqueID(ref);
+         int cReferrals = await db.get("$DB_PATH/Users/$ref.json");
+         db.patch("$DB_PATH/Users/$ref.json",{"referrals":"${++cReferrals}"});
+      }
+      db.put("$DB_PATH/Users.json", {"${userID.toString()}":MyUser(userID, upline: ref).toMap()});
+      
+     } else
+         print("User is already regis5tered");
+}
+
+Future<dynamic> getUserDetail(int userID)  =>  db.get("$DB_PATH/Users/$userID.json");
+
+
+
---
+++ src/util/user_model.dart
@@ -0,0 +1,24 @@
+import 'sensitive_info.dart' as private;
+
+// This class holds all details for user, e.g balance, referral-balance etc
+// I am using MyUser to avoid naming conflict with Telegram User class
+class MyUser{
+  final id; // this ryhmes with the user's ID on telegram to enhance uniqueness
+  String upline; // this is the id of the this user's upline (the user who referred this.user)
+  String link;
+
+  double balance = 0.000, refBal = 0, cpc; // referralBalance and (Cost Per Click)
+  int referrals = 0;
+
+  MyUser(this.id,{this.upline}): link = private.getUserUniqueID(id);   // Upline is optional because some users might not be referred
+
+  Map<String, dynamic> toMap() => {
+    "id":id,
+    "upline":upline,
+    "balance":balance,
+    "cpc":cpc,
+    "referrals":referrals,
+    "refBal":refBal,
+    "link": link
+    };
+}
